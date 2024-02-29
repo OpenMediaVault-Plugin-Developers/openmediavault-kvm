f28a7b11eef07588a3a28c98dc891eb5ba660507
Aaron Murray <plugins@omv-extras.org>
2024-02-29 06:46:55 -0600

    add omv-disk-list script to enumerate host disks
    Signed-off-by: Aaron Murray <plugins@omv-extras.org>

---
+++ usr/sbin/omv-disk-list.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# shellcheck disable=SC2155,SC2207
+#
+# Copyright (c) 2022-2024 OpenMediaVault Plugin Developers
+#
+# This file is licensed under the terms of the GNU General Public
+# License version 2. This program is licensed "as is" without any
+# warranty of any kind, whether express or implied.
+#
+
+find_symlink() {
+  local symlink=$(find "$1" -type l -lname "*$2" -not -name '*wwn-*' | head -n1)
+  if [ -n "${symlink}" ]; then
+    echo "${symlink}"
+  fi
+}
+
+disks=$(lsblk --json | jq -r '.blockdevices[] | select(.type == "disk") | .name' | awk NF)
+
+output=()
+
+for disk in ${disks}; do
+  output+=("$(find_symlink "/dev/disk/by-id/" "${disk}")")
+  output+=("$(find_symlink "/dev/disk/by-path/" "${disk}")")
+  if [ -b "/dev/${disk}" ]; then
+    output+=("/dev/${disk}")
+  fi
+done
+
+sorted=($(printf "%s\n" "${output[@]}" | sort))
+
+for entry in "${sorted[@]}"; do
+  echo "${entry}"
+done
+
+exit 0
